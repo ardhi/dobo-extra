a96f1831191c6d2aff467c63edf93c9630057127
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:39:50 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class DoboExtra extends this.lib.BajoPlugin {
+  return class DoboExtra extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'dbx'
